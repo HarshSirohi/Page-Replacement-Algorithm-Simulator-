3790be454913ea21eefa979c426b2820bd01fba4
HarshSirohi <149650943+HarshSirohi@users.noreply.github.com>
2025-02-25 22:38:11 +0530

    Add files via upload

---
+++ Basic Input & Output.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int frames, n;
+    cout << "Enter number of frames: ";
+    cin >> frames;
+    
+    cout << "Enter number of pages: ";
+    cin >> n;
+    
+    vector<int> pages(n);
+    cout << "Enter page sequence: ";
+    for (int i = 0; i < n; i++) {
+        cin >> pages[i];
+    }
+    
+    cout << "You entered: ";
+    for (int page : pages) {
+        cout << page << " ";
+    }
+    cout << endl;
+    
+    return 0;
+}
