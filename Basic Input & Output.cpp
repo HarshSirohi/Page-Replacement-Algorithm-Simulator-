5edae2a7f27150ac6fb7c8f86ac0bcd672a85a45
HarshSirohi <149650943+HarshSirohi@users.noreply.github.com>
2025-03-04 00:02:29 +0530

    Update Basic Input & Output.cpp

--- Basic Input & Output.cpp
+++ Basic Input & Output.cpp
@@ -1,5 +1,9 @@
+
 #include <iostream>
 #include <vector>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
 // Ask the user for the number of pages and frames (available memory slots).
 using namespace std;
 
@@ -32,9 +36,23 @@ int main() {
 // If memory is full, remove the oldest page (first in the queue) and add the new one.
 // Track page faults
 
-#include <queue>
-#include <unordered_set>
+// same as above with different name
+void getUserInput(vector<int>& referenceString, int& numFrames) {
+    int numPages;
+    cout << "Enter the number of pages: ";
+    cin >> numPages;
 
+    cout << "Enter the reference string (space-separated): ";
+    referenceString.resize(numPages);
+    for (int i = 0; i < numPages; i++) {
+        cin >> referenceString[i];
+    }
+
+    cout << "Enter the number of frames: ";
+    cin >> numFrames;
+}
+
+// FIFO Page Replacement Algorithm
 int fifoPageReplacement(const vector<int>& pages, int frames) {
     queue<int> memoryQueue;
     unordered_set<int> memorySet;
@@ -54,6 +72,21 @@ int fifoPageReplacement(const vector<int>& pages, int frames) {
     }
     return pageFaults;
 }
+// for displaying the result of FIFO
+void testPageReplacementAlgorithms(const vector<int>& referenceString, int numFrames) {
+    cout << "\nResults:\n";
+    cout << "FIFO Page Faults: " << fifoPageReplacement(referenceString,numFrames) << "\n";
+}
+
+int main() {
+    vector<int> referenceString;
+    int numFrames;
+    getUserInput(referenceString, numFrames);
+    testPageReplacementAlgorithms(referenceString, numFrames);
+
+    return 0;
+}
+
 
 
 
