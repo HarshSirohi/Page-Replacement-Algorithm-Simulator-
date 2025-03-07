95e46a75ec34eb6776315b1c272ae2353a5d85cf
HarshSirohi <149650943+HarshSirohi@users.noreply.github.com>
2025-03-07 20:38:54 +0530

    Update Basic Input & Output.cpp

--- Basic Input & Output.cpp
+++ Basic Input & Output.cpp
@@ -30,6 +30,7 @@ int main() {
     return 0;
 }
 
+// 1st algo FIFO
 // Using Algorithm FIFO (First-In-First-Out) in which a queue to track the order of pages added.
 // When a new page arrives:
 // If there is space in memory, add it.
@@ -72,6 +73,42 @@ int fifoPageReplacement(const vector<int>& pages, int frames) {
     }
     return pageFaults;
 }
+
+
+// 2nd algo (LRU)
+// it removes the least recently used page.
+
+
+#include <unordered_map>
+
+int lruPageReplacement(const vector<int>& pages, int frames) {
+    vector<int> memory;
+    unordered_map<int, int> lastUsed;
+    int pageFaults = 0;
+
+    for (int i = 0; i < pages.size(); i++) {
+        int page = pages[i];
+
+        auto it = find(memory.begin(), memory.end(), page);
+        if (it == memory.end()) {  // Page not found in memory
+            if (memory.size() == frames) {  // If memory is full, remove LRU page
+                int lruIndex = 0;
+                for (int j = 1; j < memory.size(); j++) {
+                    if (lastUsed[memory[j]] < lastUsed[memory[lruIndex]]) {
+                        lruIndex = j;
+                    }
+                }
+                memory.erase(memory.begin() + lruIndex);
+            }
+            memory.push_back(page);
+            pageFaults++;
+        }
+        lastUsed[page] = i;  // Update last used time
+    }
+    return pageFaults;
+}
+
+
 // for displaying the result of FIFO
 void testPageReplacementAlgorithms(const vector<int>& referenceString, int numFrames) {
     cout << "\nResults:\n";
